ffdad31ea55e6c2183651d6349a9764fbd60ed13
sam-n-johnston <sam.n.johnston@gmail.com>
2018-02-23 12:36:28 -0500

    Create another section

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Spacer from './components/Spacer'
 import FullWidthImage from './components/FullWidthImage'
 import FadeSection from './components/FadeSection'
 
-const sectionHeights = [250, 10000000]
+const sectionHeights = [250, 1650, 10000000]
 
 export class ReactNativeWeb extends Component {
   constructor (props) {
@@ -42,13 +42,15 @@ export class ReactNativeWeb extends Component {
   }
 
   setSection (distanceToTop: number) {
+    console.log('set section')
+    console.log(distanceToTop)
     if (distanceToTop < sectionHeights[0]) {
       this.setState({ currentSection: 0 })
     } else if (distanceToTop < sectionHeights[1]) {
       this.setState({ currentSection: 1 })
-    } else if (distanceToTop < sectionHeights[1]) {
+    } else if (distanceToTop < sectionHeights[2]) {
       this.setState({ currentSection: 2 })
-    } else if (distanceToTop < sectionHeights[1]) {
+    } else if (distanceToTop < sectionHeights[2]) {
       this.setState({ currentSection: 3 })
     }
   }
@@ -121,12 +123,25 @@ export class ReactNativeWeb extends Component {
     )
   }
 
+  renderFixedStuff () {
+    return <Video />
+    //   if (this.state.currentSection === 0) {
+    //   return <Video />
+    // } else if (this.state.currentSection === 2) {
+    //   return <Video />
+    // }
+  }
+
   render () {
+    console.log('render')
+    console.log(this.state.currentSection)
     return (
       <View >
         <View style={styles.fixedContainer}>
-          <FadeSection isVisible={this.state.currentSection === 0} >
-            <Video />
+          <FadeSection
+            isVisible={this.state.currentSection === 0 || this.state.currentSection === 2}
+          >
+            {this.renderFixedStuff()}
           </FadeSection>
         </View>
         <ScrollView style={styles.container} >
@@ -136,7 +151,7 @@ export class ReactNativeWeb extends Component {
             {this.renderText(text1)}
           </FadeSection>
           <Spacer height={1000} />
-          {this.renderText(text1)}
+          {this.renderText(text2)}
         </ScrollView>
       </View>
     );
@@ -170,3 +185,6 @@ let text1 = `Identity is such an unstable concept, isn’t it?
   
   And how labyrinthine that negotiation has become against the backdrop of today’s cultural turbulence and technological acceleration. The virtual has expanded our sense of self into a new digital layer, which creates new modes by which our identity may be expressed, fragmented, and understood.`
 
+let text2 = `Identity as spectacle. Two socially disparate worlds surface to mind: fashion and cosplay. Cosplay refers to the activity of dressing up as a fictional character, matching everything from hairstyle to clothing to even iconic mannerisms. The practice draws from a rich literary and graphic lineage of video games, comics, anime, and more.
+ 
+Both fashion and cosplay revel in participating in a fantastical world that has been envisioned by an artist, such as fashion designer Rei Kawakubo’s Comme Des Garçons or video game designer Yoko Taro’s Nier: Automata. Both are primarily concerned with clothing and styling as means by which to participate and express one’s identity (assumed or otherwise).`
